c05ebe47988024712d34647152b9f084446f3d1b
Gabriel Pereira <pereiradosanjosgabriel@gmail.com>
2025-05-29 23:45:33 -0300

    Add files via upload

--- portifólio/static/script.js
+++ portifólio/static/script.js
@@ -20,6 +20,40 @@ infoLinks.forEach(link => {
   });
 });
 
+let sobrePereira = false
+let linguagensEFerramentas = false
+
+
+
 function mostrarInfo(infoId) {
-  
+
+  document.getElementById(infoId)
+  if (infoId === 'sobre-pereira' && sobrePereira === false) {
+    document.getElementById('conteudo-apresentacao').style.display = "block"
+    document.getElementById('conteudo-linguagens-e-ferramentas').style.display = "none"
+    sobrePereira = true
+
+  } else if (infoId === 'linguagens-e-ferramentas' && linguagensEFerramentas === false) {
+    document.getElementById('conteudo-apresentacao').style.display = "none"
+    document.getElementById('conteudo-linguagens-e-ferramentas').style.display = "block"
+    linguagensEFerramentas = true
+
+  } else if (infoId === 'sobre-pereira' && sobrePereira === true) {
+    document.getElementById('conteudo-apresentacao').style.display = "none"
+    sobrePereira = false
+
+  } else if (infoId === 'linguagens-e-ferramentas' && linguagensEFerramentas === true) {
+    document.getElementById('conteudo-linguagens-e-ferramentas').style.display = "none"
+    linguagensEFerramentas = false
+
+  }
+
 }
+
+document.getElementById("sobre-pereira").addEventListener("click", () => {
+  mostrarInfo("sobre-pereira")
+})
+
+document.getElementById("linguagens-e-ferramentas").addEventListener("click", () => {
+  mostrarInfo("linguagens-e-ferramentas")
+})
